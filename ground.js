291b27e31f2cf6b5937e693bf749e8c41b8cb5ad
MuhammadMisbahLone <74707768+MuhammadMisbahLone@users.noreply.github.com>
2020-12-10 14:57:26 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,11 @@
+class Ground{
+constructor() {
+ var goptions = {isStatic:true};
+ this.body = Bodies.rectangle(200,380,400,40,goptions);
+ World.add(world,this.body);
+}
+display() {
+ rectMode(CENTER);
+ rect(this.body.position.x,this.body.position.y,400,40);
+}
+}
